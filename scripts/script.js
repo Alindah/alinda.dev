d7c9adfa589b08b5eee54b8d753758bdd896dd9b
Alinda <AlindaHeng@gmail.com>
2020-09-01 16:50:42 -0700

    Added "Contact" page.

--- scripts/script.js
+++ scripts/script.js
@@ -33,16 +33,21 @@ function updateActivePage(element) {
 	// How many pixels away from element top before we trigger the tab switch.
 	var tolerance = window.screen.height * 0.15;
 
+	// Positions of top of each page.
 	var homePosition = document.getElementById("home");
 	var aboutPosition = document.getElementById("about");
 	var expPosition = document.getElementById("experience");
+	var conPosition = document.getElementById("contact");
 
+	// How close you need to scroll before it registers as a new page.
 	if (Math.abs(element.scrollTop - homePosition.offsetTop) < tolerance)
 		switchPage(document.getElementById("nav-home"));
 	else if (Math.abs(element.scrollTop - aboutPosition.offsetTop) < tolerance)
 		switchPage(document.getElementById("nav-about"));
 	else if (Math.abs(element.scrollTop - expPosition.offsetTop) < tolerance)
 		switchPage(document.getElementById("nav-experience"));
+	else if (Math.abs(element.scrollTop - conPosition.offsetTop) < tolerance)
+		switchPage(document.getElementById("nav-contact"));
 }
 
 function switchPage(element) {
